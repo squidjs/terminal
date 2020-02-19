09c64f5e52b801c98b091012371d269ac2c8915c
Tom LIENARD <quiibzdev@gmail.com>
2020-02-19 17:31:36 +0100

    Add comments to Settings.ts

--- app/settings/Settings.ts
+++ app/settings/Settings.ts
@@ -14,11 +14,21 @@ export default class Settings {
         this.settings = this.load();
     }
 
+    /**
+     * Get the value of a key
+     * @param key
+     * @return The value from the key
+     */
     get(key: string): string | boolean | number {
 
         return this.settings[key];
     }
 
+    /**
+     * Set the value for a key
+     * @param key
+     * @param value
+     */
     set(key: string, value: object) {
 
         this.settings[key] = value;
@@ -26,6 +36,10 @@ export default class Settings {
         save(this.path, this.settings);
     }
 
+    /**
+     * Load the settings
+     * @return The loaded settings
+     */
     load(): ISettings {
 
         try {
@@ -39,6 +53,11 @@ export default class Settings {
     }
 }
 
+/**
+ * Save the settings to a file
+ * @param path
+ * @param settings
+ */
 function save(path: string, settings: ISettings) {
 
     fs.writeFileSync(path, JSON.stringify(settings));
@@ -46,18 +65,52 @@ function save(path: string, settings: ISettings) {
 
 export interface ISettings {
 
+    /**
+     * The theme interface
+     */
     theme: ITheme;
+
+    /**
+     * The cursor interface
+     */
     cursor: ICursor;
+
+    /**
+     * The font interface
+     */
     font: IFont;
+
+    /**
+     * The background image interface
+     */
     backgroundImage: IBackgroundImage;
+
+    /**
+     * A path to the bash you which to use
+     */
     bash: string;
+
+    /**
+     * The name of the current theme
+     */
     currentTheme: string;
+
+    /**
+     * Use or not the experimental char atlas
+     */
     experimentalCharAtlas: 'none' | 'static' | 'dynamic';
 }
 
 interface ITheme {
 
+    /**
+     * The theme name
+     */
     name: string;
+
+    /**
+     * Bellow are the colors of the theme
+     */
     background: string;
     foreground: string;
     cursor: string;
@@ -65,18 +118,39 @@ interface ITheme {
 
 interface ICursor {
 
+    /**
+     * The style of the cursor
+     */
     style: 'block' | 'underline' | 'bar';
+
+    /**
+     * Does the cursor should blink or not
+     */
     blink: boolean;
 }
 
 interface IFont {
 
+    /**
+     * The size of the font in the terminal
+     */
     size: number;
+
+    /**
+     * The family of the font in the terminal
+     */
     family: string;
 }
 
 interface IBackgroundImage {
 
+    /**
+     * The path to the background image
+     */
     path: string;
+
+    /**
+     * The opacity of the background image
+     */
     opacity: number;
 }
