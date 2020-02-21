da31872b7931d441116cd6a1994ce53b0ea84f99
Tom LIENARD <quiibzdev@gmail.com>
2020-02-21 12:49:44 +0100

    Remove tabs system

--- app/renderer.ts
+++ app/renderer.ts
@@ -1,48 +1,4 @@
-import * as TabGroup from 'electron-tabs';
-import * as dragula from 'dragula';
 import { ipcRenderer, remote } from 'electron';
-
-let tabGroup = new TabGroup({
-
-    newTab: {
-        title: 'Terminal',
-        src: '../ui/terminal.html',
-        webviewAttributes: {
-          nodeIntegration: 'true'
-        },
-        visible: true,
-        active: true
-    },
-    ready: (tabGroup) => {
-
-        dragula([tabGroup.tabContainer], {
-
-            direction: 'horizontal'
-        });
-    }
-});
-
-tabGroup.addTab({
-
-    title: 'Terminal',
-    src: '../ui/terminal.html',
-    webviewAttributes: {
-        nodeIntegration: 'true'
-    },
-    visible: true,
-    active: true
-});
-
-tabGroup.on('tab-removed', (tab, tabGroup) => {
-
-    if(tabGroup.getActiveTab() == null)
-        remote.getCurrentWindow().close();
-});
-
-tabGroup.on('tab-added', (tab, tabGroup) => {
-
-    tab.webview.addEventListener("dom-ready", (event) => { tab.webview.blur(); tab.webview.focus(); });
-});
 
 ipcRenderer.on('shortcuts', (event, message) => {
 
@@ -53,37 +9,12 @@ ipcRenderer.on('shortcuts', (event, message) => {
     switch (message) {
 
         case 'closeTab':
-
-            if(tabGroup.getActiveTab().getTitle() == "Welcome" && tabGroup.getTabByRelPosition(1) == null && tabGroup.getTabByRelPosition(-1) == null)
-                remote.getCurrentWindow().close();
-            else
-                tabGroup.getActiveTab().close(true);
             break;
 
         case 'openTab':
-
-            tabGroup.addTab({
-
-                title: 'Terminal',
-                src: '../ui/terminal.html',
-                webviewAttributes: {
-                    nodeIntegration: 'true'
-                },
-                visible: true,
-                active: true
-            });
           break;
 
         case 'switchTab':
-
-            let tab = tabGroup.getTabByRelPosition(1);
-
-            if(tab != null)
-                tab.activate();
-            else
-                tab = tabGroup.getTabByPosition(1);
-
-            tab.activate();
           break;
     }
 });
