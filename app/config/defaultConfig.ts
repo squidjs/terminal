fbe874f2efe0d416f7019ed0449e4dd7e928f4d6
Tom LIENARD <quiibzdev@gmail.com>
2020-02-21 18:26:51 +0100

    Add colors to themes

--- app/config/defaultConfig.ts
+++ app/config/defaultConfig.ts
@@ -4,8 +4,24 @@ export const defaultConfig: ISettings = {
     theme: {
         name: 'default',
         background: '#0F0F0F',
-        foreground: '#3fac42',
-        cursor: '#3fac42'
+        foreground: '#22da6e',
+        cursor: '#22da6e',
+        black: '#011627',
+        red: '#EF5350',
+        green: '#22da6e',
+        yellow: '#addb67',
+        blue: '#82aaff',
+        magenta: '#c792ea',
+        cyan: '#21c7a8',
+        white: '#ffffff',
+        lightBlack: '#575656',
+        lightRed: '#ef5350',
+        lightGreen: '#22da6e',
+        lightYellow: '#ffeb95',
+        lightBlue: '#82aaff',
+        lightMagenta: '#c792ea',
+        lightCyan: '#7fdbca',
+        lightWhite: '#ffffff'
     },
     cursor: {
         style: 'block',
--- app/settings/Settings.ts
+++ app/settings/Settings.ts
@@ -132,6 +132,22 @@ interface ITheme {
     background: string;
     foreground: string;
     cursor: string;
+    black: string;
+    red: string;
+    green: string;
+    yellow: string;
+    blue: string;
+    magenta: string;
+    cyan: string;
+    white: string;
+    lightBlack: string;
+    lightRed: string;
+    lightGreen: string;
+    lightYellow: string;
+    lightBlue: string;
+    lightMagenta: string;
+    lightCyan: string;
+    lightWhite: string;
 }
 
 interface ICursor {
