0a38078842d79925c064ba98ae9eff86a18b1014
Tom LIENARD <quiibzdev@gmail.com>
2021-01-14 17:07:29 +0100

    fix: Add file protocol to bg images and fix ssh icons

--- app/resolvers/icon/resolvers/TitleIconResolver.ts
+++ app/resolvers/icon/resolvers/TitleIconResolver.ts
@@ -1,11 +1,9 @@
 import { IWindow } from '@app/Terminal';
 import { Resolver } from '@common/resolvers/Resolver';
 import { UndefinedObject } from '@common/types/types';
-import { IconResolverType, SSH_ICON } from '@app/resolvers/icon/IconResolverProvider';
+import { IconResolverType } from '@app/resolvers/icon/IconResolverProvider';
 
 export default class TitleIconResolver implements Resolver<IWindow, IconResolverType> {
-
-    private readonly SSH_REGEX = /^[a-z]+@([a-z]|[A-Z]|[0-9])+:/;
 
     /**
      * Resolve the icon based on the name of the IWindow instance.
@@ -28,8 +26,6 @@ export default class TitleIconResolver implements Resolver<IWindow, IconResolver
             icon = ['dev-javascript', '#E8D44D'];
         else if(name.includes('npm'))
             icon = ['dev-npm', '#C53635'];
-        else if(this.SSH_REGEX.test(name))
-            icon = SSH_ICON;
 
         // Languages
         else if(name.includes('php'))
