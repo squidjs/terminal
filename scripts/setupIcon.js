2cf889c03d27dfdf8ff533d4dc3c7ac65c7b61fd
Tom <quiibzdev@gmail.com>
2021-03-13 15:57:17 +0100

    build: Fix icon setup script

--- scripts/setupIcon.js
+++ scripts/setupIcon.js
@@ -4,7 +4,7 @@ console.log('Fetching app version...');
 
 const package = require('../package.json');
 const path = require('path');
-const shell = require('child_process').execSync;
+const copydir = require('copy-dir');
 
 const isCanary = package.version.includes('canary');
 
@@ -12,7 +12,5 @@ const iconDir =  'build';
 const targetDir = path.join(iconDir, isCanary ? 'canary' : 'default');
 
 console.log(`Copying ${targetDir} to ${iconDir}...`);
-
-shell(`cp ${targetDir}/* ${iconDir}`);
-
+copydir.sync(targetDir, iconDir);
 console.log('Done!');
