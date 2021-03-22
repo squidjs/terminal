76bd43764afff811661b4a49c85806f62c73e72b
Tom <quiibzdev@gmail.com>
2021-03-22 08:45:06 +0100

    feat: Verify package before installing

--- cli/src/commands/install.ts
+++ cli/src/commands/install.ts
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 import { getConfig, PACKAGES_PATH, saveConfig } from '../utils/config';
 import { spawn } from 'child_process';
 import { YARN_PATH } from '../utils/utils';
+import fetch from 'node-fetch';
 
 export const install: CommandModule = {
 
@@ -30,30 +31,56 @@ export const install: CommandModule = {
         }
 
         console.log(' ');
-        console.log(`Installing ${packageName} in ${chalk.green(PACKAGES_PATH)}...`);
-        console.log(' ');
+        console.log(`Fetching informations about ${packageName}...`);
 
-        const install = spawn(process.execPath, [YARN_PATH, 'add', '--cwd', process.env.HOME, '--modules-folder', '.squid', packageName], { stdio: 'inherit', env: process.env });
+        fetch(`https://api.npms.io/v2/package/${packageName}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        }).then(r => r.json()).then((data) => {
 
-        install.on('message', (msg) => console.log(msg))
-        install.on('exit', (code) => {
+            if(data.code && data.code === 'NOT_FOUND') {
 
-            if(code !== 0) {
-
-                console.log('Installation failed!')
+                console.log(chalk.red('Package not found.'));
                 return;
             }
 
-            const config = getConfig();
-            saveConfig({
-                ...config,
-                packages: [...config.packages, packageName],
-            });
+            if(!data.collected.metadata.keywords.includes('squid-package')) {
+
+                console.log(chalk.red('This package is not a Squid package.'));
+                return;
+            }
 
             console.log(' ');
-            console.log('Success!');
+            console.log(`Installing ${packageName} in ${chalk.green(PACKAGES_PATH)}...`);
+            console.log(` Version: ${chalk.blue(data.collected.metadata.version)}`);
+            console.log(` Author: ${chalk.blue(data.collected.metadata.publisher.username)}`);
+            console.log(' ');
 
-            process.exit(0);
+            const install = spawn(process.execPath, [YARN_PATH, 'add', '--cwd', process.env.HOME, '--modules-folder', '.squid', packageName], { stdio: 'inherit', env: process.env });
+
+            install.on('message', (msg) => console.log(msg))
+            install.on('exit', (code) => {
+
+                if(code !== 0) {
+
+                    console.log('Installation failed!')
+                    return;
+                }
+
+                const config = getConfig();
+                saveConfig({
+                    ...config,
+                    packages: [...config.packages, packageName],
+                });
+
+                console.log(' ');
+                console.log('Success!');
+
+                process.exit(0);
+            });
         });
     },
 };
