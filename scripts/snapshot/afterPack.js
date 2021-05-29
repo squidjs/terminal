db1276e02d0cbcb07f5ecf97d342d009bbeb8a2f
Tom <quiibzdev@gmail.com>
2021-05-29 18:56:21 +0200

    feat: Add snapshot

---
+++ scripts/snapshot/afterPack.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const fs = require('fs')
+
+const isArm = require('os').arch === 'arm64';
+
+exports.default = async (context) => {
+    const baseDirPath = path.resolve(__dirname)
+    const snapshotName = isArm ? 'v8_context_snapshot.bin' : 'v8_context_snapshot.arm64.bin';
+    const pathToBlob = path.join(baseDirPath, snapshotName)
+
+    switch (process.platform) {
+      case 'darwin': {
+        const macDist = fs.existsSync(path.resolve(__dirname, '..', '..', 'dist', 'mac-arm64'))
+            ? 'mac-arm64'
+            : 'mac';
+
+        const pathToElectron = path.resolve(
+          __dirname,
+          '..',
+          '..',
+          `dist/${macDist}/Squid.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Resources`
+        )
+        fs.copyFileSync(pathToBlob, path.join(pathToElectron, snapshotName))
+        break
+      }
+      // TODO
+      case 'win32':
+      case 'linux': {
+        break
+      }
+    }
+}
---
+++ scripts/snapshot/copy.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const fs = require('fs')
+
+const baseDirPath = path.resolve(__dirname)
+const snapshotName = require('os').arch === 'arm64' ? 'v8_context_snapshot.bin' : 'v8_context_snapshot.arm64.bin';
+const pathToBlob = path.join(baseDirPath, snapshotName)
+
+switch (process.platform) {
+  case 'darwin': {
+    const pathToElectron = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'node_modules/electron/dist/Electron.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Resources'
+    )
+    fs.copyFileSync(pathToBlob, path.join(pathToElectron, snapshotName))
+    break
+  }
+  case 'win32':
+  case 'linux': {
+    const pathToElectron = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'node_modules',
+      'electron',
+      'dist'
+    )
+    fs.copyFileSync(pathToBlob, path.join(pathToElectron, snapshotName))
+    break
+  }
+}
